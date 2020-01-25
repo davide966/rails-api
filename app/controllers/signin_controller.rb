5dae5479c89fe657862b7e6301bf42084cbb81e0
davide966 <dabuck@libero.it>
2020-01-25 17:00:40 +0100

    Fino a user creation

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -8,9 +8,9 @@ class SigninController < ApplicationController
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       tokens = session.login
       response.set_cookie(JWTSessions.access_cookie,
-                        value: tokens[:access],
-                        httponly: true,
-                        secure: Rails.env.production?)
+                          value: tokens[:access],
+                          httponly: true,
+                          secure: Rails.env.production?)
       render json: { csrf: tokens[:csrf] }
     else
       not_authorized
@@ -26,6 +26,6 @@ class SigninController < ApplicationController
   private
 
   def not_found
-    render json: { error: "Cannot find email/password combination" }, status: :not_found
+    render json: { error: 'Cannot find email/password combination' }, status: :not_found
   end
 end
--- daxdocs/dax.md
+++ daxdocs/dax.md
@@ -1,4 +1,5 @@
+https://web-crunch.com/ruby-on-rails-api-vue-js/
+
 **git config --global user.name "davide966"**
 **git config --global user.email "dabuck@libero.it"**
 **git remote add origin**
-azz
